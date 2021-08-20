0742c810ba8bc19e83266776981c9ae15347338f
donah <dona.hsieh@gmail.com>
2021-08-20 16:12:29 -0700

    Add route not found view in case of 404 error

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
+import RouteNotFound from '../views/RouteNotFound.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -19,6 +20,12 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'RouteNotFound',
+    // eslint-disable-next-line no-undef
+    component: RouteNotFound,
   },
 ]
 
---
+++ src/views/RouteNotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>No such page can be found at this time.</h1>
+  </div>
+</template>
