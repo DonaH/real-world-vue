8350688605c7ec6751cbe3e8fd046fc6ecb98021
donah <dona.hsieh@gmail.com>
2021-09-02 18:54:10 -0700

    Add redirects & alias routes. Add Register Event button.

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,19 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register My Event</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // call to API
+      // If registered then redirect to event details
+      this.$router.push({
+        name: 'EventDetails',
+      })
+    },
+  },
 }
 </script>
